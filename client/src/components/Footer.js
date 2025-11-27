ccf1ba2e21cd296ca16a7de5e50cf408f0ad2933
maryam.almaani <maryam.almaani@gmail.com>
2025-11-28 00:54:35 +0400

    updated header and footer to match prototype

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,8 +1,21 @@
 const Footer = () => {
     return (
-        <div className="footer">
-            Copyrights reserverd to UTAS@2025
+      <div className="footer">
+        <h3>Blood Donation System</h3>
+  
+        <div className="footer-info">
+          <p>Blood.Donation@BloodDonation.com</p>
+          <p>+968 24875532</p>
+          <p>Muscat, Oman</p>
+          <p>PO BOX 122</p>
         </div>
-    )
-}
-export default Footer;
+  
+        <p className="footer-copy">
+          © 2024 BloodDonation Inc.
+        </p>
+      </div>
+    );
+  };
+  
+  export default Footer;
+  
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,43 +5,63 @@ import {
     NavbarBrand,
     Nav,
     NavItem,
-    NavLink,
-    UncontrolledDropdown,
-    DropdownToggle,
-    DropdownMenu,
-    DropdownItem,
-    NavbarText,
 } from 'reactstrap';
 import { useState } from 'react';
-import Logo from '../assets/logo.png';
 import { Link } from 'react-router-dom';
-import { FaHome, FaUserAlt,FaSignOutAlt   } from "react-icons/fa";
+import Logo from '../assets/logo.png';
 
 const Header = () => {
     const [isOpen, setIsOpen] = useState(false);
     const toggle = () => setIsOpen(!isOpen);
+
     return (
         <>
-            <Navbar className='navigation' light expand='md'>
+            <Navbar className='navigation' light expand='md' style={styles.navbar}>
                 <NavbarBrand>
-                    <img src={Logo} width="150px" height="75px"/>
+                    <img src={Logo} width="150px" height="75px" alt="logo" />
                 </NavbarBrand>
+
                 <NavbarToggler onClick={toggle} />
+
                 <Collapse isOpen={isOpen} navbar>
                     <Nav className="ms-auto" navbar>
+
                         <NavItem className='navs'>
-                            <Link to=""><FaHome /></Link>
+                            <Link to="/signin" style={styles.link}>Sign in</Link>
                         </NavItem>
+
                         <NavItem className='navs'>
-                            <Link to=""><FaUserAlt /></Link>
+                            <Link to="/signup" style={styles.link}>Sign up</Link>
                         </NavItem>
+
                         <NavItem className='navs'>
-                            <Link to=""><FaSignOutAlt /></Link>
+                            <Link to="/services" style={styles.link}>Services</Link>
                         </NavItem>
+
+                        <NavItem className='navs'>
+                            <Link to="/about" style={styles.link}>About Us</Link>
+                        </NavItem>
+
                     </Nav>
                 </Collapse>
             </Navbar>
         </>
-    )
-}
+    );
+};
+
 export default Header;
+
+const styles = {
+    navbar: {
+        backgroundColor: "#FDF4F4",
+        borderBottom: "2px solid #E19E9C",
+        padding: "10px 20px",
+    },
+    link: {
+        textDecoration: "none",
+        marginRight: "20px",
+        color: "#B3261E",
+        fontWeight: "600",
+        fontSize: "16px",
+    }
+};
