f9156d06b2e134f91e4e78c7ee615293a9bf5863
Razan Al Raiisi <razanraiisi@gmail.com>
2025-12-04 13:09:28 +0400

    added landing page + about us page +did some modifications in header and home

--- client/src/App.js
+++ client/src/App.js
@@ -2,28 +2,33 @@ import './App.css';
 import Login from './components/Login.js';
 import Register from './components/Register.js';
 import Home from './components/Home.js';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { Container, Row } from 'reactstrap';
 import Header from './components/Header.js';
-import { useSelector } from 'react-redux';
 import Footer from './components/Footer.js';
 import RequestBlood from './components/RequestBlood.js';
 import DonationCenter from './components/DonationCenter.js';
 import DonateBlood from './components/DonateBlood.js';
 import User from './components/User.js';
 import AboutUs from './components/AboutUs.js';
+import LandingPage from './components/LandingPage.js';
 
-function App() {
-  const email=useSelector((state)=>state.users.user.email);
+function AppContent() {
+  const location = useLocation();
+
+  
+  const hideOn = ["/", "/login", "/register"]; 
+
   return (
     <Container fluid className='appBG'>
-      <Router>
-        <Row>
-          <Header/>
-        </Row>
-        <Row>
-          <Routes>
-          <Route path="/" element={<Home />} />
+      <Row>
+        {!hideOn.includes(location.pathname) && <Header />}
+      </Row>
+
+      <Row>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/request" element={<RequestBlood />} />
@@ -32,12 +37,20 @@ function App() {
           <Route path="/UserProfile" element={<User />} />
           <Route path="/AboutUs" element={<AboutUs />} />
         </Routes>
-        </Row>
-        <Row>
-          <Footer/>
-        </Row>
-      </Router>
+      </Row>
+
+      <Row>
+        {!hideOn.includes(location.pathname) && <Footer />}
+      </Row>
     </Container>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <AppContent />
+    </Router>
   );
 }
 
--- client/src/components/AboutUs.js
+++ client/src/components/AboutUs.js
@@ -1,7 +1,18 @@
+import { Col, Row } from "reactstrap";
+import bloodDonorCartoon from "../assets/bloodDonorCartoon.jpg";
+
 const AboutUs = () => {
     return (
-        <div> 
-            <h1>About Us</h1>
+        <div className="justify-content-center text-center p-4">
+            <Row><h1 style={{ color:'#B3261E'}}>About Us</h1></Row><br/><br/>
+            <Row>
+                <Col><img alt="Blood Donation" style={{width:'80%'}} src={bloodDonorCartoon} /></Col>
+                <Col>
+            <p>BloodLink is a dedicated blood donation platform based in Oman, designed to seamlessly connect donors with recipients in need. Our mission is to save lives by making blood donation and requests simple, 
+                efficient, and accessible to everyone across the country. Through BloodLink, users can easily register to donate, request blood, and locate donation centers, all in one place. We prioritize transparency, 
+                safety, and timely communication, ensuring that every donation reaches those who need it most. By fostering a strong community of volunteers and leveraging modern technology, 
+                BloodLink strives to enhance the culture of blood donation in Oman and make a meaningful impact on public health.</p></Col></Row> 
+            
         </div>
     );
 }
