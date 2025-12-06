d158d73a4a695d05e804340b832c9a60055d09b1
Razan Al Raiisi <razanraiisi@gmail.com>
2025-12-06 13:17:39 +0400

    added mre validations for sign in and sign up

--- client/src/validations/userSchemaValidation.js
+++ client/src/validations/userSchemaValidation.js
@@ -1,6 +1,13 @@
 import * as yup from 'yup';
 
 export const UserSchemaValidation = yup.object().shape({
-    email:yup.string().email('Not a Valid Email Format!!').required('Email is Required..'),
-    password:yup.string().required('Password is Required..').min(4,'Minimum 4 characters required..').max(8,'Maximum 8 characters required..'),
+    email: yup
+        .string()
+        .email('Invalid email format! Please use example@mail.com')
+        .required('Email is required.'),
+    password: yup
+        .string()
+        .required('Password is required.')
+        .min(4, 'Password must be at least 4 characters.')
+        .max(8, 'Password cannot exceed 8 characters.'),
 });
